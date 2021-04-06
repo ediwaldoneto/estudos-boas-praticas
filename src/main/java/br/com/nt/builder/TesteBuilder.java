c8bfcd8c3e9f5ac0ea43c38a3a58e8b3ef9c9da8
Ediwaldo Neto <ediwaldo.neto@outlook.com>
2021-04-06 10:30:35 -0300

    adicionado log4j

--- src/main/java/br/com/nt/builder/TesteBuilder.java
+++ src/main/java/br/com/nt/builder/TesteBuilder.java
@@ -1,12 +1,18 @@
 package br.com.nt.builder;
 
+import org.apache.log4j.Logger;
+
 public class TesteBuilder {
+
+	private static final Logger LOGGER = Logger.getLogger(TesteBuilder.class);
 
 	public static void main(String[] args) {
 
 		Venda venda = new VendaBuilder().paraComprador("Neto").comDocumento("2345678")
 				.comProduto(new Produto("TV LG 42", 3000.0)).comProduto(new Produto("Notebook DELL", 6000.0))
 				.naDataAtual().controi();
+
+		LOGGER.info(venda.toString());
 
 	}
 }
